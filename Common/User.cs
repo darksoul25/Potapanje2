8a566c98742ab3d417b91ad50aca20d6da18a190
darksoul25 <147412589+darksoul25@users.noreply.github.com>
2023-12-25 18:51:24 +0100

    Update Test2

--- Common/User.cs
+++ Common/User.cs
@@ -13,5 +13,6 @@ namespace Common
         public  string Email { get; set; }
         public  string Password { get; set; }
         public string Test { get; set; }
+        public string Test2 {get; set;}
     }
 }
